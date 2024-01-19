4bd252bb5314718fc96cfdb84d544ee64987a343
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2024-01-19 13:30:03 +0100

    removed old folder

--- modules/generated_liteeth/Manifest.py
+++
@@ -1,3 +0,0 @@
-files = [
-    "build/gateware/liteeth_core.v",
-]
--- modules/generated_liteeth/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf build/*
-
-export PYTHONPATH="../liteeth:../liteiclink"
-../liteeth/liteeth/gen.py --no-compile Nexys4Ethernet_UDP.yml
