8d551f167b26d298743ef27fc4581a3684c3f31a
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2024-01-19 13:04:33 +0100

    do not compile anything

--- modules/generated_liteeth/generate.sh
+++ modules/generated_liteeth/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf build/*
 
 export PYTHONPATH="../liteeth:../liteiclink"
-../liteeth/liteeth/gen.py Nexys4Ethernet_UDP.yml
+../liteeth/liteeth/gen.py --no-compile Nexys4Ethernet_UDP.yml
