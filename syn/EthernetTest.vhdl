acbcf889f358f9da63a590818da6467cd681d6b5
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2024-01-22 11:53:20 +0100

    updated liteeth to raw packet handling

--- syn/EthernetTest.vhdl
+++ syn/EthernetTest.vhdl
@@ -85,64 +85,84 @@ ARCHITECTURE Behavioral OF EthernetTest IS
     ----------------------------------------------------------------------------
     -- CLOCKS
     ----------------------------------------------------------------------------
-    SIGNAL CLK50MHZ               : STD_LOGIC                                := '0';
+    SIGNAL CLK50MHZ                    : STD_LOGIC                                := '0';
 
     ----------------------------------------------------------------------------
     -- RESET
     ----------------------------------------------------------------------------
-    SIGNAL reset_cnt              : INTEGER RANGE 0 TO RESET_RELEASE_CNT + 1 := 0; -- counter for releasing the reset signal
-    SIGNAL sys_reset              : STD_LOGIC                                := '1';
+    SIGNAL reset_cnt                   : INTEGER RANGE 0 TO RESET_RELEASE_CNT + 1 := 0; -- counter for releasing the reset signal
+    SIGNAL sys_reset                   : STD_LOGIC                                := '1';
 
     ----------------------------------------------------------------------------
     -- SIGNALS
     ----------------------------------------------------------------------------
-    SIGNAL streamer1_ip_address   : STD_LOGIC_VECTOR(31 DOWNTO 0)            := (OTHERS => '0');
-    SIGNAL streamer1_sink_data    : STD_LOGIC_VECTOR(31 DOWNTO 0)            := (OTHERS => '0');
-    SIGNAL streamer1_sink_last    : STD_LOGIC                                := '0';
-    SIGNAL streamer1_sink_ready   : STD_LOGIC                                := '0';
-    SIGNAL streamer1_sink_valid   : STD_LOGIC                                := '0';
+    -- signals for FPGA -> PC
+    SIGNAL streamer1_sink_data         : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_sink_dst_port     : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_sink_ip_address   : STD_LOGIC_VECTOR (31 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_sink_last         : STD_LOGIC                                := '0';
+    SIGNAL streamer1_sink_last_be      : STD_LOGIC_VECTOR (1 DOWNTO 0)            := (OTHERS => '0');
+    SIGNAL streamer1_sink_length       : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_sink_ready        : STD_LOGIC                                := '0';
+    SIGNAL streamer1_sink_src_port     : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_sink_valid        : STD_LOGIC                                := '0';
 
-    SIGNAL streamer1_source_data  : STD_LOGIC_VECTOR(31 DOWNTO 0)            := (OTHERS => '0');
-    SIGNAL streamer1_source_error : STD_LOGIC                                := '0';
-    SIGNAL streamer1_source_last  : STD_LOGIC                                := '0';
-    SIGNAL streamer1_source_ready : STD_LOGIC                                := '0';
-    SIGNAL streamer1_source_valid : STD_LOGIC                                := '0';
+    -- signals for PC -> FPGA
+    SIGNAL streamer1_source_data       : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_source_dst_port   : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_source_error      : STD_LOGIC                                := '0';
+    SIGNAL streamer1_source_ip_address : STD_LOGIC_VECTOR (31 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_source_last       : STD_LOGIC                                := '0';
+    SIGNAL streamer1_source_last_be    : STD_LOGIC_VECTOR (1 DOWNTO 0)            := (OTHERS => '0');
+    SIGNAL streamer1_source_length     : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_source_ready      : STD_LOGIC                                := '0';
+    SIGNAL streamer1_source_src_port   : STD_LOGIC_VECTOR (15 DOWNTO 0)           := (OTHERS => '0');
+    SIGNAL streamer1_source_valid      : STD_LOGIC                                := '0';
 
-    CONSTANT streamer1_udp_port   : STD_LOGIC_VECTOR(15 DOWNTO 0)            := x"00FF";
-
-    CONSTANT fpga_mac             : STD_LOGIC_VECTOR (47 DOWNTO 0)           := x"00183e01ff71"; -- FPGA's MAC address
-    CONSTANT fpga_ip              : STD_LOGIC_VECTOR (31 DOWNTO 0)           := x"C0A8010C";     -- FPGA's IP4 address 192.168.1.12
+    CONSTANT fpga_mac                  : STD_LOGIC_VECTOR (47 DOWNTO 0)           := x"00183e01ff71"; -- FPGA's MAC address
+    CONSTANT fpga_ip                   : STD_LOGIC_VECTOR (31 DOWNTO 0)           := x"C0A8010C";     -- FPGA's IP4 address 192.168.1.12
     ----------------------------------------------------------------------------
     -- liteeth core
     ----------------------------------------------------------------------------
     COMPONENT liteeth_core IS
         PORT (
-            ip_address             : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-            mac_address            : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+            ip_address                  : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            mac_address                 : IN STD_LOGIC_VECTOR (47 DOWNTO 0);
 
-            rmii_clocks_ref_clk    : IN STD_LOGIC;
-            rmii_crs_dv            : IN STD_LOGIC;
-            rmii_mdc               : OUT STD_LOGIC;
-            rmii_mdio              : INOUT STD_LOGIC;
-            rmii_rst_n             : OUT STD_LOGIC;
-            rmii_rx_data           : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-            rmii_tx_data           : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
-            rmii_tx_en             : OUT STD_LOGIC;
-            streamer1_ip_address   : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-            streamer1_sink_data    : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-            streamer1_sink_last    : IN STD_LOGIC;
-            streamer1_sink_ready   : OUT STD_LOGIC;
-            streamer1_sink_valid   : IN STD_LOGIC;
-            streamer1_source_data  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-            streamer1_source_error : OUT STD_LOGIC;
-            streamer1_source_last  : OUT STD_LOGIC;
-            streamer1_source_ready : IN STD_LOGIC;
-            streamer1_source_valid : OUT STD_LOGIC;
-            streamer1_udp_port     : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-            sys_clock              : IN STD_LOGIC;
-            sys_reset              : IN STD_LOGIC
+            rmii_clocks_ref_clk         : IN STD_LOGIC;
+            rmii_crs_dv                 : IN STD_LOGIC;
+            rmii_mdc                    : OUT STD_LOGIC;
+            rmii_mdio                   : INOUT STD_LOGIC;
+            rmii_rst_n                  : OUT STD_LOGIC;
+            rmii_rx_data                : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+            rmii_tx_data                : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+            rmii_tx_en                  : OUT STD_LOGIC;
+
+            streamer1_sink_data         : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_sink_dst_port     : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_sink_ip_address   : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            streamer1_sink_last         : IN STD_LOGIC;
+            streamer1_sink_last_be      : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+            streamer1_sink_length       : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_sink_ready        : OUT STD_LOGIC;
+            streamer1_sink_src_port     : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_sink_valid        : IN STD_LOGIC;
+
+            streamer1_source_data       : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_source_dst_port   : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_source_error      : OUT STD_LOGIC;
+            streamer1_source_ip_address : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+            streamer1_source_last       : OUT STD_LOGIC;
+            streamer1_source_last_be    : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+            streamer1_source_length     : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_source_ready      : IN STD_LOGIC;
+            streamer1_source_src_port   : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+            streamer1_source_valid      : OUT STD_LOGIC;
+
+            sys_clock                   : IN STD_LOGIC;
+            sys_reset                   : IN STD_LOGIC
         );
-    END COMPONENT;
+    END COMPONENT; -- liteeth_core
     ----------------------------------------------------------------------------
 
 BEGIN
@@ -150,40 +170,47 @@ BEGIN
     liteeth_core_0 : liteeth_core
     PORT MAP(
         -- reference clock of the PHY
-        rmii_clocks_ref_clk    => CLK50MHZ,
+        rmii_clocks_ref_clk         => CLK50MHZ,
 
         -- RMII interface to the PHY
-        rmii_crs_dv            => PhyCrs,
-        rmii_mdc               => PhyMdc,
-        rmii_mdio              => PhyMdio,
-        rmii_rst_n             => PhyRstn,
-        rmii_rx_data           => PhyRxd,
-        rmii_tx_data           => PhyTxd,
-        rmii_tx_en             => PhyTxEn,
+        rmii_crs_dv                 => PhyCrs,
+        rmii_mdc                    => PhyMdc,
+        rmii_mdio                   => PhyMdio,
+        rmii_rst_n                  => PhyRstn,
+        rmii_rx_data                => PhyRxd,
+        rmii_tx_data                => PhyTxd,
+        rmii_tx_en                  => PhyTxEn,
 
         -- FPGA to PC interface
-        streamer1_ip_address   => streamer1_ip_address,
-        streamer1_sink_data    => streamer1_sink_data,
-        streamer1_sink_last    => streamer1_sink_last,
-        streamer1_sink_ready   => streamer1_sink_ready,
-        streamer1_sink_valid   => streamer1_sink_valid,
+        streamer1_sink_data         => streamer1_sink_data,
+        streamer1_sink_dst_port     => streamer1_sink_dst_port,
+        streamer1_sink_ip_address   => streamer1_sink_ip_address,
+        streamer1_sink_last         => streamer1_sink_last,
+        streamer1_sink_last_be      => streamer1_sink_last_be,
+        streamer1_sink_length       => streamer1_sink_length,
+        streamer1_sink_ready        => streamer1_sink_ready,
+        streamer1_sink_src_port     => streamer1_sink_src_port,
+        streamer1_sink_valid        => streamer1_sink_valid,
 
         -- PC to FPGA interface
-        streamer1_source_data  => streamer1_source_data,
-        streamer1_source_error => streamer1_source_error,
-        streamer1_source_last  => streamer1_source_last,
-        streamer1_source_ready => streamer1_source_ready,
-        streamer1_source_valid => streamer1_source_valid,
-
-        streamer1_udp_port     => streamer1_udp_port,
+        streamer1_source_data       => streamer1_source_data,
+        streamer1_source_dst_port   => streamer1_source_dst_port,
+        streamer1_source_error      => streamer1_source_error,
+        streamer1_source_ip_address => streamer1_source_ip_address,
+        streamer1_source_last       => streamer1_source_last,
+        streamer1_source_last_be    => streamer1_source_last_be,
+        streamer1_source_length     => streamer1_source_length,
+        streamer1_source_ready      => streamer1_source_ready,
+        streamer1_source_src_port   => streamer1_source_src_port,
+        streamer1_source_valid      => streamer1_source_valid,
 
         -- configuration of IP and MAC
-        ip_address             => fpga_ip,
-        mac_address            => fpga_mac,
+        ip_address                  => fpga_ip,
+        mac_address                 => fpga_mac,
 
         -- system clock and the system reset
-        sys_clock              => CLK100MHZ,
-        sys_reset              => sys_reset
+        sys_clock                   => CLK100MHZ,
+        sys_reset                   => sys_reset
     );
 
     RGB1_Blue  <= streamer1_sink_valid;
@@ -193,18 +220,7 @@ BEGIN
     RGB2_Green <= streamer1_source_ready;
     RGB2_Red   <= streamer1_source_error;
 
-    UDP_RX : PROCESS (CLK50MHZ) BEGIN
-        IF rising_edge(CLK50MHZ) THEN
-            IF (streamer1_source_ready = '0') THEN
-                streamer1_source_ready <= '1';
-            END IF;
-
-            IF streamer1_source_valid = '1' THEN
-                led                    <= streamer1_source_data(15 DOWNTO 0);
-                streamer1_source_ready <= '0';
-            END IF;
-        END IF;
-    END PROCESS;
+    led <= streamer1_source_dst_port;
 
     -- generate the 50MHz clock needed for the PHY
     RMII_clk : PROCESS (CLK100MHZ) BEGIN
