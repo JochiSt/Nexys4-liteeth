f5612eca3fa7ef59279f59d5a55bf78d9cdf6c90
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2024-01-22 16:40:00 +0100

    removed also the .backup.log files

--- syn/Manifest.py
+++ syn/Manifest.py
@@ -20,7 +20,7 @@ syn_project = "EthernetTest"
 syn_tool = "vivado"
 
 # when done with bitstream generation, remove jou and log files from vivado
-syn_pre_synthesis_cmd = "rm -rf *.jou *.log"
+syn_pre_synthesis_cmd = "rm -rf *.jou *.log *.backup.log"
 syn_post_bitstream_cmd = "rm -rf *.jou"
 
 syn_properties = [
