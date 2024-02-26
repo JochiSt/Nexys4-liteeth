566690989771b672fc248e145b0ebf1e48a3bef6
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2024-02-26 14:04:39 +0100

    removed conversion from Verilog to VHDL (it's now included in generate.sh)

--- modules/Ethernet/Manifest.py
+++ modules/Ethernet/Manifest.py
@@ -13,12 +13,9 @@ print("#"*80, file=sys.stderr)
 
 subprocess.run(["./generate.sh"], shell=True, cwd=path)
 
-subprocess.run(["python pyVHDLinstTemplate.py"], shell=True, cwd=os.path.join( path, "../../utils/pyVHDLinstTemplate"))
-
 print("#"*80, file=sys.stderr)
 print("done", file=sys.stderr)
 print("#"*80, file=sys.stderr)
-
 
 files = [
     "build/gateware/liteeth_core.v",
