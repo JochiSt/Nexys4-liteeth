b0bb2a04cf13ba941f7c0452f995d3dc1572d784
JochiSt <jochen.steinmann@web.de>
2026-01-29 12:06:10 +0100

    adjusted to current generated liteeth core

--- syn/UDP2LED/UDP2LED.vhdl
+++ syn/UDP2LED/UDP2LED.vhdl
@@ -8,26 +8,27 @@ ENTITY UDP2LED IS
 
     );
     PORT (
-        CLK100MHZ   : IN STD_LOGIC;
+        CLK100MHZ : IN STD_LOGIC;
 
-        PhyRxd      : INOUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-        PhyTxd      : INOUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-        PhyCrs      : INOUT STD_LOGIC;
-        PhyTxEn     : INOUT STD_LOGIC;
-        PhyRxErr    : INOUT STD_LOGIC;
+        PhyRxd   : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+        PhyTxd   : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+        PhyCrs   : IN STD_LOGIC;
+        PhyTxEn  : OUT STD_LOGIC;
+        PhyRxErr : IN STD_LOGIC;
+
         PhyMdc      : OUT STD_LOGIC;
         PhyMdio     : INOUT STD_LOGIC;
         PhyClk50Mhz : OUT STD_LOGIC;
         PhyRstn     : OUT STD_LOGIC;
 
         -- display signals for ARP and UDP packets
-        RGB1_Blue   : OUT STD_LOGIC;
-        RGB1_Green  : OUT STD_LOGIC;
-        RGB1_Red    : OUT STD_LOGIC;
+        RGB1_Blue  : OUT STD_LOGIC;
+        RGB1_Green : OUT STD_LOGIC;
+        RGB1_Red   : OUT STD_LOGIC;
 
-        RGB2_Blue   : OUT STD_LOGIC;
-        RGB2_Green  : OUT STD_LOGIC;
-        RGB2_Red    : OUT STD_LOGIC;
+        RGB2_Blue  : OUT STD_LOGIC;
+        RGB2_Green : OUT STD_LOGIC;
+        RGB2_Red   : OUT STD_LOGIC;
 
         -- Reset and SMI inputs/outputs
         btnCpuReset : IN STD_LOGIC;
@@ -40,82 +41,14 @@ ARCHITECTURE Behavioral OF UDP2LED IS
     ----------------------------------------------------------------------------
     -- CLOCKS
     ----------------------------------------------------------------------------
-    SIGNAL CLK50MHZ              : STD_LOGIC                                     := '0';
-    SIGNAL CLK125MHz             : STD_LOGIC                                     := '0';
+    SIGNAL CLK50MHZ : STD_LOGIC := '0';
 
     ----------------------------------------------------------------------------
     -- RESET
     ----------------------------------------------------------------------------
-    SIGNAL reset_cnt             : INTEGER RANGE 0 TO RESET_RELEASE_CNT + 1      := 0; -- counter for releasing the reset signal
-    SIGNAL sys_reset             : STD_LOGIC                                     := '1';
-
+    SIGNAL reset_cnt : INTEGER RANGE 0 TO RESET_RELEASE_CNT + 1 := 0; -- counter for releasing the reset signal
+    SIGNAL reset_n   : STD_LOGIC                                := '0';
     ----------------------------------------------------------------------------
-    -- SIGNALS
-    ----------------------------------------------------------------------------
-    CONSTANT ETH_DATA_WIDTH      : INTEGER                                       := 32;
-    -- signals for FPGA -> PC
-    SIGNAL udp_sink_data         : STD_LOGIC_VECTOR(ETH_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL udp_sink_dst_port     : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_sink_ip_address   : STD_LOGIC_VECTOR(31 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_sink_last         : STD_LOGIC                                     := '0';
-    SIGNAL udp_sink_last_be      : STD_LOGIC_VECTOR(3 DOWNTO 0)                  := (OTHERS => '0');
-    SIGNAL udp_sink_length       : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_sink_ready        : STD_LOGIC                                     := '0';
-    SIGNAL udp_sink_src_port     : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_sink_valid        : STD_LOGIC                                     := '0';
-
-    SIGNAL udp_source_data       : STD_LOGIC_VECTOR(ETH_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL udp_source_dst_port   : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_source_error      : STD_LOGIC                                     := '0';
-    SIGNAL udp_source_ip_address : STD_LOGIC_VECTOR(31 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_source_last       : STD_LOGIC                                     := '0';
-    SIGNAL udp_source_last_be    : STD_LOGIC_VECTOR(3 DOWNTO 0)                  := (OTHERS => '0');
-    SIGNAL udp_source_length     : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_source_ready      : STD_LOGIC                                     := '0';
-    SIGNAL udp_source_src_port   : STD_LOGIC_VECTOR(15 DOWNTO 0)                 := (OTHERS => '0');
-    SIGNAL udp_source_valid      : STD_LOGIC                                     := '0';
-
-    CONSTANT fpga_mac            : STD_LOGIC_VECTOR (47 DOWNTO 0)                := x"00_18_3e_01_ff_71"; -- FPGA's MAC address
-    CONSTANT fpga_ip             : STD_LOGIC_VECTOR (31 DOWNTO 0)                := x"C0_A8_01_0C";       -- FPGA's IP4 address 192.168.1.12
-    ----------------------------------------------------------------------------
-    -- liteeth core
-    ----------------------------------------------------------------------------
-    COMPONENT liteeth_core IS
-        PORT (
-            rmii_clocks_ref_clk   : IN STD_LOGIC;
-            rmii_crs_dv           : IN STD_LOGIC;
-            rmii_mdc              : OUT STD_LOGIC;
-            rmii_mdio             : INOUT STD_LOGIC;
-            rmii_rst_n            : OUT STD_LOGIC;
-            rmii_rx_data          : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-            rmii_tx_data          : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-            rmii_tx_en            : OUT STD_LOGIC;
-
-            sys_clock             : IN STD_LOGIC;
-            sys_reset             : IN STD_LOGIC;
-
-            udp_sink_data         : IN STD_LOGIC_VECTOR (ETH_DATA_WIDTH - 1 DOWNTO 0);
-            udp_sink_dst_port     : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_sink_ip_address   : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-            udp_sink_last         : IN STD_LOGIC;
-            udp_sink_last_be      : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-            udp_sink_length       : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_sink_ready        : OUT STD_LOGIC;
-            udp_sink_src_port     : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_sink_valid        : IN STD_LOGIC;
-
-            udp_source_data       : OUT STD_LOGIC_VECTOR (ETH_DATA_WIDTH - 1 DOWNTO 0);
-            udp_source_dst_port   : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_source_error      : OUT STD_LOGIC;
-            udp_source_ip_address : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            udp_source_last       : OUT STD_LOGIC;
-            udp_source_last_be    : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            udp_source_length     : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_source_ready      : IN STD_LOGIC;
-            udp_source_src_port   : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-            udp_source_valid      : OUT STD_LOGIC
-        );
-    END COMPONENT; -- liteeth_core
 
     COMPONENT Ethernet_PLL IS
         PORT (
@@ -126,114 +59,60 @@ ARCHITECTURE Behavioral OF UDP2LED IS
         );
     END COMPONENT;
 
-    COMPONENT readEthernetPacket IS
-        GENERIC (
-            PORT_MSB   : NATURAL := 102;
-            DATA_WIDTH : NATURAL := 16
-        );
+    SIGNAL CLK125MHz : STD_LOGIC := '0';
+    ----------------------------------------------------------------------------
+    -- liteeth core (from instatiation template)
+    ----------------------------------------------------------------------------
+    COMPONENT liteeth_core IS
         PORT (
-            clk                   : IN STD_LOGIC;
-            reset                 : IN STD_LOGIC;
+            rmii_clocks_ref_clk : IN STD_LOGIC;
+            rmii_crs_dv         : IN STD_LOGIC;
+            rmii_mdc            : OUT STD_LOGIC;
+            rmii_mdio           : INOUT STD_LOGIC;
+            rmii_rst_n          : OUT STD_LOGIC;
+            rmii_rx_data        : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+            rmii_tx_data        : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+            rmii_tx_en          : OUT STD_LOGIC;
 
-            udp_source_valid      : IN STD_LOGIC;
-            udp_source_last       : IN STD_LOGIC;
-            udp_source_ready      : OUT STD_LOGIC;
+            sys_clock : IN STD_LOGIC;
+            sys_reset : IN STD_LOGIC;
 
-            udp_source_src_port   : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-            udp_source_dst_port   : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-            udp_source_ip_address : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            udp0_sink_data  : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+            udp0_sink_last  : IN STD_LOGIC;
+            udp0_sink_ready : OUT STD_LOGIC;
+            udp0_sink_valid : IN STD_LOGIC;
 
-            udp_source_length     : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-            udp_source_data       : IN STD_LOGIC_VECTOR(ETH_DATA_WIDTH - 1 DOWNTO 0);
-
-            udp_source_error      : IN STD_LOGIC;
-            led                   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+            udp0_source_data  : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+            udp0_source_error : OUT STD_LOGIC;
+            udp0_source_last  : OUT STD_LOGIC;
+            udp0_source_ready : IN STD_LOGIC;
+            udp0_source_valid : OUT STD_LOGIC
         );
     END COMPONENT;
 
-    SIGNAL ETH_CLK : STD_LOGIC := '0';
-    ----------------------------------------------------------------------------
+    --------------------------------------------------------------------------------
+    -- SIGNAL templates
+    --------------------------------------------------------------------------------
+    SIGNAL sys_clock : STD_LOGIC := '0';
+    SIGNAL sys_reset : STD_LOGIC := '0';
+
+    -- FPGA to PHY (sending)
+    SIGNAL udp0_sink_data  : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL udp0_sink_last  : STD_LOGIC                    := '0';
+    SIGNAL udp0_sink_ready : STD_LOGIC                    := '0';
+    SIGNAL udp0_sink_valid : STD_LOGIC                    := '0';
+
+    -- PHY to FPGA (receiving)
+    SIGNAL udp0_source_data  : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL udp0_source_error : STD_LOGIC                    := '0';
+    SIGNAL udp0_source_last  : STD_LOGIC                    := '0';
+    SIGNAL udp0_source_ready : STD_LOGIC                    := '0';
+    SIGNAL udp0_source_valid : STD_LOGIC                    := '0';
+
 BEGIN
 
     ----------------------------------------------------------------------------
-    -- select the master clock for all Ethernet things
-    ETH_CLK <= CLK100MHz;
-    -- ETH_CLK <= CLK125MHz;
-    ----------------------------------------------------------------------------
-
-    liteeth_core_0 : liteeth_core
-    PORT MAP(
-        -- reference clock of the PHY
-        rmii_clocks_ref_clk   => CLK50MHZ,
-
-        -- RMII interface to the PHY
-        rmii_crs_dv           => PhyCrs,
-        rmii_mdc              => PhyMdc,
-        rmii_mdio             => PhyMdio,
-        rmii_rst_n            => PhyRstn,
-        rmii_rx_data          => PhyRxd,
-        rmii_tx_data          => PhyTxd,
-        rmii_tx_en            => PhyTxEn,
-
-        sys_clock             => ETH_CLK,
-        sys_reset             => sys_reset,
-
-        udp_sink_data         => udp_sink_data,
-        udp_sink_dst_port     => udp_sink_dst_port,
-        udp_sink_ip_address   => udp_sink_ip_address,
-        udp_sink_last         => udp_sink_last,
-        udp_sink_last_be      => udp_sink_last_be,
-        udp_sink_length       => udp_sink_length,
-        udp_sink_ready        => udp_sink_ready,
-        udp_sink_src_port     => udp_sink_src_port,
-        udp_sink_valid        => udp_sink_valid,
-
-        udp_source_data       => udp_source_data,
-        udp_source_dst_port   => udp_source_dst_port,
-        udp_source_error      => udp_source_error,
-        udp_source_ip_address => udp_source_ip_address,
-        udp_source_last       => udp_source_last,
-        udp_source_last_be    => udp_source_last_be,
-        udp_source_length     => udp_source_length,
-        udp_source_ready      => udp_source_ready,
-        udp_source_src_port   => udp_source_src_port,
-        udp_source_valid      => udp_source_valid
-    );
-
-    readEthernetPacket_0 : readEthernetPacket
-    GENERIC MAP(
-        PORT_MSB   => 102,
-        DATA_WIDTH => ETH_DATA_WIDTH
-    )
-    PORT MAP(
-        clk                   => ETH_CLK,
-        reset                 => sys_reset,
-
-        udp_source_valid      => udp_source_valid,
-        udp_source_last       => udp_source_last,
-        udp_source_ready      => udp_source_ready,
-
-        udp_source_src_port   => udp_source_src_port,
-        udp_source_dst_port   => udp_source_dst_port,
-        udp_source_ip_address => udp_source_ip_address,
-
-        udp_source_length     => udp_source_length,
-        udp_source_data       => udp_source_data,
-
-        udp_source_error      => udp_source_error,
-
-        led                   => led
-    );
-
-    --RGB1_Blue  <= streamer1_sink_valid;
-    --RGB1_Green <= streamer1_sink_ready;
-
-    RGB2_Blue  <= udp_source_valid;
-    RGB2_Green <= udp_source_ready;
-    RGB2_Red   <= udp_source_error;
-
-    --led        <= streamer1_source_dst_port;
-
+    -- clock
     -- generate  50 MHz clock needed for the PHY
     -- generate 200 MHz for the liteeth
     Ethernet_PLL_0 : Ethernet_PLL
@@ -243,21 +122,73 @@ BEGIN
         CLK125MHz => CLK125MHz,
         CLK50MHz  => CLK50MHZ
     );
-
     -- forward the 50MHz clock to the PHY
     PhyClk50Mhz <= CLK50MHZ;
+    sys_clock   <= CLK100MHZ;
 
+    ----------------------------------------------------------------------------
+    -- RESET
     -- a simple process removing the reset condition
     proc_reset : PROCESS (clk100MHz) BEGIN
         IF rising_edge(clk100MHz) THEN
-            IF sys_reset = '1' THEN
+            IF reset_n = '0' THEN
                 IF reset_cnt < RESET_RELEASE_CNT THEN
                     reset_cnt <= reset_cnt + 1;
                 ELSE
-                    sys_reset <= '0';
+                    reset_n <= '1';
                 END IF;
             END IF;
         END IF;
     END PROCESS proc_reset;
 
+    sys_reset <= NOT reset_n;
+
+    ----------------------------------------------------------------------------
+    -- liteeth core instantiation
+    liteeth_core_0 : liteeth_core
+    PORT MAP(
+        rmii_clocks_ref_clk => CLK50MHZ,
+        rmii_rst_n          => PhyRstn,
+        rmii_mdc            => PhyMdc,
+        rmii_mdio           => PhyMdio,
+
+        rmii_rx_data => PhyRxd,
+        rmii_crs_dv  => PhyCrs,
+
+        rmii_tx_data => PhyTxd,
+        rmii_tx_en   => PhyTxEn,
+
+        sys_clock => sys_clock,
+        sys_reset => sys_reset,
+
+        udp0_sink_data  => udp0_sink_data,
+        udp0_sink_last  => udp0_sink_last,
+        udp0_sink_ready => udp0_sink_ready,
+        udp0_sink_valid => udp0_sink_valid,
+
+        udp0_source_data  => udp0_source_data,
+        udp0_source_error => udp0_source_error,
+        udp0_source_last  => udp0_source_last,
+        udp0_source_ready => udp0_source_ready,
+        udp0_source_valid => udp0_source_valid
+    );
+
+    ----------------------------------------------------------------------------
+    -- UDP to LED writer instantiation
+    UDP2LED_inst : ENTITY work.UDP_led_writer
+        PORT MAP(
+            -- system clock and reset
+            clk     => CLK100MHZ,
+            reset_n => reset_n,
+
+            -- data from liteeth UDP core
+            udp_source_valid => udp0_source_valid,
+            udp_source_last  => udp0_source_last,
+            udp_source_ready => udp0_source_ready,
+            udp_source_data  => udp0_source_data,
+            udp_source_error => udp0_source_error,
+
+            -- output to the LEDs
+            leds => led
+        );
 END Behavioral;
