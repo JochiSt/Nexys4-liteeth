6c34197f73eaa6f95b8eac6498d3f01695d10864
JochiSt <jochen.steinmann@web.de>
2026-01-29 10:50:10 +0100

    adapted source code to match receiving routine

--- modules/Ethernet/readEthernetPacket.vhdl
+++ modules/Ethernet/readEthernetPacket.vhdl
@@ -1,92 +1,73 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
-ENTITY readEthernetPacket IS
-    GENERIC (
-        PORT_MSB   : NATURAL := 102;
-        DATA_WIDTH : NATURAL := 16 -- WIDTH of the data to be handled
-    );
+--------------------------------------------------------------------------------
+-- UDP to LED writer
+-- receives UDP packets and writes data to on Board LEDs
+--------------------------------------------------------------------------------
+ENTITY UDP_led_writer IS
     PORT (
-        clk                   : IN STD_LOGIC;
-        reset                 : IN STD_LOGIC;
+        -- system clock and reset
+        clk   : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
 
-        udp_source_valid      : IN STD_LOGIC;
-        udp_source_last       : IN STD_LOGIC;
-        udp_source_ready      : OUT STD_LOGIC;
+        -- data from liteeth UDP core
+        udp0_source_valid : IN STD_LOGIC;
+        udp0_source_last  : IN STD_LOGIC;
+        udp0_source_ready : OUT STD_LOGIC;
+        udp0_source_data  : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        udp0_source_error : IN STD_LOGIC;
 
-        udp_source_src_port   : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        udp_source_dst_port   : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        udp_source_ip_address : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-
-        udp_source_length     : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        udp_source_data       : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
-
-        udp_source_error      : IN STD_LOGIC;
-
-        led                   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        -- output to the LEDs
+        leds : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
+END UDP_led_writer;
 
-END ENTITY;
+ARCHITECTURE arch OF UDP_led_writer IS
+    TYPE UDP_state_t IS (STATE_WAIT_PACKET, STATE_READ_DATA);
+    SIGNAL UDP_state : UDP_state_t := STATE_WAIT_PACKET;
 
-ARCHITECTURE implementation OF readEthernetPacket IS
-    TYPE UDP_STATE_T IS (STATE_WAIT_PACKET, STATE_READ_DATA);
-    SIGNAL udp_state : UDP_STATE_T := STATE_WAIT_PACKET;
-
-    SIGNAL leds      : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL data : STD_LOGIC_VECTOR(15 DOWNTO 0);
 BEGIN
 
-    -- PROCESS (clk)
-    -- BEGIN
-    --     IF rising_edge(clk) THEN
-    --         IF reset = '1' THEN
-    --             leds(1 DOWNTO 0) <= (OTHERS => '0');
-    --             udp_source_ready <= '0';
-    --             udp_state        <= STATE_WAIT_PACKET;
-    --         ELSE
-    --             CASE udp_state IS
-    --                 WHEN STATE_WAIT_PACKET =>
-    --                     udp_source_ready <= '1';
-    --                     leds(1 DOWNTO 0) <= "01";
-    --                     IF udp_source_valid = '1' THEN
-    --                         IF udp_source_last = '0' THEN
-    --                             udp_state <= STATE_READ_DATA;
-    --                         END IF;
-    --                     END IF;
-
-    --                 WHEN STATE_READ_DATA =>
-    --                     leds(1 DOWNTO 0) <= "10";
-    --                     IF udp_source_valid = '1' THEN
-    --                         IF udp_source_last = '1' THEN
-    --                             udp_state <= STATE_WAIT_PACKET;
-    --                         END IF;
-    --                     END IF;
-    --             END CASE;
-    --         END IF;
-    --     END IF;
-    -- END PROCESS;
-
-    udp_source_ready <= '1'; -- always ready
-
     PROCESS (clk) BEGIN
-        IF rising_edge(clk) THEN
-            IF reset = '1' THEN
-                leds(15 DOWNTO 13) <= (OTHERS => '0');
+
+        IF risign_edge(clk) THEN
+            -- RESET
+            IF (reset) THEN
+                udp0_source_ready <= '0';
+                UDP_state         <= STATE_WAIT_PACKET;
+                data              <= (OTHERS => '0');
             ELSE
-                IF leds(15) = '0' THEN
-                    leds(15) <= udp_source_valid;
-                END IF;
-                IF leds(14) = '0' THEN
-                    leds(14) <= udp_source_last;
-                END IF;
-                IF leds(13) = '0' THEN
-                    leds(13) <= udp_source_error;
-                END IF;
+                -- handle data
+                CASE UDP_state IS
+                    WHEN STATE_WAIT_PACKET =>
+                        udp0_source_ready <= '1'; -- signal ready to receive data
+                        IF (udp0_source_valid = '1') THEN
+                            IF (udp0_source_last = '0') THEN -- force at least 2 bytes
+                                data(15 DOWNTO 8) <= udp0_source_data;
+                                UDP_state         <= STATE_READ_DATA;
+                            END IF;
+                        END IF;
+
+                    WHEN STATE_READ_DATA =>
+                        IF (udp0_source_valid = '1') THEN
+                            data(7 DOWNTO 0) <= udp0_source_data;
+                            UDP_state        <= STATE_READ_DATA;
+                        END IF;
+
+                        IF (udp0_source_last = '1') THEN -- if only 2 bytes, last will be high here
+                            udp0_source_ready <= '0';        -- stop receiving data
+                            UDP_state         <= STATE_WAIT_PACKET;
+                        END IF;
+                END CASE;
             END IF;
+
+            -- assign LEDs to output
+            leds <= data;
         END IF;
+
     END PROCESS;
 
-    leds(12 DOWNTO 0) <= udp_source_data(12 DOWNTO 0);
-
-    led               <= leds;
-
-END;
+END ARCHITECTURE; -- arch
